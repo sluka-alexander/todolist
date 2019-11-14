c279d565bbc08525cdf77b81548b1688983519e8
Sluka Alexander <slukasane@gmail.com>
2019-11-14 18:30:49 +0300

    completed html output of the task to the website

--- script.js
+++ script.js
@@ -10,10 +10,29 @@ const button = document.getElementById('add_new_task');
 let list = [];
 let id = 1;
 
+function add_task_html(title, text) {
+    const item = `<div class="content">
+        <div class="content_outside">
+            <div class="label"></div>
+            <div class="circle-border"></div>
+            <div class="item">
+                <div class="title_item">
+                    <div class="title">${title}</div>
+                    <div class="delete"></div>
+                </div>
+            </div>
+            <div class="text">${text}</div>
+        </div>
+    </div>`;
+    const position = 'beforeend';
+    tasks.insertAdjacentHTML(position, item);
+}
+
 function add_task() {
     const title_value = title.value;
     const text_value = text.value;
     if (title_value && text_value) {
+        add_task_html(title.value,text.value);
         list.push({
             title: title.value,
             text: text.value,
@@ -24,6 +43,8 @@ function add_task() {
     id++;
     title.value = '';
     text.value = '';
+    grey.style.display = 'none';
+    form_task.style.display = 'none';
 }
 
 button.addEventListener('click', add_task);
