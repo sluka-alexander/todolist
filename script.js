86f7b7577040cc8e93974b289897b635a8c03169
Sluka Alexander <slukasane@gmail.com>
2019-11-14 17:39:51 +0300

    created the appearance of the form when you click on the button

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const date = document.getElementById("date");
+const open_form = document.getElementById('open_form');
+const grey = document.getElementById('grey');
+const form_task = document.getElementById('form');
+
+function show_form(state) {
+        form_task.style.display = state;
+        grey.style.display = state;
+}
 
 // Show todays date
 function date_today() {
-    const options = {weekday : "short", month:"short", day:"numeric"};
+    const options = {weekday: "short", month: "short", day: "numeric"};
     const today = new Date();
 
     date.innerHTML = today.toLocaleDateString("eng-ENG", options);
 }
+
 date_today();
