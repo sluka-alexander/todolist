cdcbb36bf7b00473d46bda2aeec610ee854a2fd0
Sluka Alexander <slukasane@gmail.com>
2019-11-16 15:17:54 +0300

    done label for html

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const date = document.getElementById("date");
 const grey = document.getElementById('grey');
 const form_task = document.getElementById('form');
+const check_items = document.getElementById('check_items');
+
 
 const title = document.getElementById('title');
 const tasks = document.getElementById('tasks');
 const text = document.getElementById('text');
 const button = document.getElementById('add_new_task');
-const button_cleaner = document.getElementById('cleaner')
+const button_cleaner = document.getElementById('cleaner');
 
 let list = [];
 let id = 1;
@@ -26,10 +28,10 @@ function clean_all_tasks() {
     })
 }
 
-function add_task_html(title, text, id_task) {
+function add_task_html(title, text, id_task,color_label) {
     const item = `<div class="content">
         <div class="content_outside">
-            <div class="label"></div>
+            <div class="label ${color_label}"></div>
             <div class="circle-border"></div>
             <div class="item">
                 <div class="title_item">
@@ -46,15 +48,16 @@ function add_task_html(title, text, id_task) {
 
 form_task.addEventListener("click", (event)=> {
     let id_task = event.target.id;
-    label = id_task;
-    console.log(id_task);
+    if (event.target.className != 'radio_button') return;
+        label = id_task;
+        console.log(label);
 });
 
 function add_task() {
     const title_value = title.value;
     const text_value = text.value;
     if (title_value && text_value) {
-        add_task_html(title.value,text.value,list.length + 1);
+        add_task_html(title.value,text.value,list.length + 1, label);
         list.push({
             title: title.value,
             text: text.value,
@@ -62,14 +65,18 @@ function add_task() {
             label: label
         });
         console.log(list);
+        grey.style.display = 'none';
+        form_task.style.display = 'none';
+        check_items.style.display = 'none';
+        title.value = '';
+        text.value = '';
+    }
+    else {
+        check_items.style.display = 'block';
     }
     id++;
     localStorage.setItem('todo', JSON.stringify(list));
     localStorage.setItem('id', JSON.stringify(id));
-    title.value = '';
-    text.value = '';
-    grey.style.display = 'none';
-    form_task.style.display = 'none';
 }
 
 button.addEventListener('click', add_task);
@@ -83,11 +90,12 @@ document.addEventListener("keyup",function(event){
 function show_form(state) {
     form_task.style.display = state;
     grey.style.display = state;
+    check_items.style.display = 'none';
 }
 function out_all_task() {
     let id_task = 1;
     for (let i in list){
-        add_task_html(list[i].title,list[i].text,id_task);
+        add_task_html(list[i].title,list[i].text,id_task,list[i].label);
         id_task++;
     }
 }
@@ -107,6 +115,8 @@ tasks.addEventListener('click',(event)=>{
     list.splice(id_task -1 , 1);
     localStorage.setItem('todo', JSON.stringify(list));
 });
+
+
 
 date_today();
 clean_all_tasks();
