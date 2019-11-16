9f2a884f310e7741527c7d58cef8956e89ce4654
Sluka Alexander <slukasane@gmail.com>
2019-11-16 21:59:46 +0300

    add showing sorts task

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const grey = document.getElementById('grey');
 const form_task = document.getElementById('form');
 const check_items = document.getElementById('check_items');
 
+// const block_select = document.getElementById('container_sort');
+const sort_tasks = document.getElementById('sort_tasks');
 
 const title = document.getElementById('title');
 const tasks = document.getElementById('tasks');
@@ -14,7 +16,7 @@ let list = [];
 let id = 1;
 let label = ' ';
 
-if(localStorage.getItem('todo')!= undefined){
+if (localStorage.getItem('todo') != undefined) {
     list = JSON.parse(localStorage.getItem('todo'));
     id = JSON.parse(localStorage.getItem('id'));
     console.log(list);
@@ -22,13 +24,13 @@ if(localStorage.getItem('todo')!= undefined){
 }
 
 function clean_all_tasks() {
-    button_cleaner.addEventListener('click', ()=>{
+    button_cleaner.addEventListener('click', () => {
         localStorage.clear();
         window.location.reload();
     })
 }
 
-function add_task_html(title, text, id_task,color_label) {
+function add_task_html(title, text, id_task, color_label) {
     const item = `<div class="content">
         <div class="content_outside">
             <div class="label ${color_label}"></div>
@@ -46,18 +48,18 @@ function add_task_html(title, text, id_task,color_label) {
     tasks.insertAdjacentHTML(position, item);
 }
 
-form_task.addEventListener("click", (event)=> {
+form_task.addEventListener("click", (event) => {
     let id_task = event.target.id;
     if (event.target.className != 'radio_button') return;
-        label = id_task;
-        console.log(label);
+    label = id_task;
+    console.log(label);
 });
 
 function add_task() {
     const title_value = title.value;
     const text_value = text.value;
     if (title_value && text_value) {
-        add_task_html(title.value,text.value,list.length + 1, label);
+        add_task_html(title.value, text.value, list.length + 1, label);
         list.push({
             title: title.value,
             text: text.value,
@@ -70,8 +72,7 @@ function add_task() {
         check_items.style.display = 'none';
         title.value = '';
         text.value = '';
-    }
-    else {
+    } else {
         check_items.style.display = 'block';
     }
     id++;
@@ -81,8 +82,8 @@ function add_task() {
 
 button.addEventListener('click', add_task);
 
-document.addEventListener("keyup",function(event){
-    if(event.keyCode == 13){
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
         add_task();
     }
 });
@@ -92,10 +93,11 @@ function show_form(state) {
     grey.style.display = state;
     check_items.style.display = 'none';
 }
+
 function out_all_task() {
     let id_task = 1;
-    for (let i in list){
-        add_task_html(list[i].title,list[i].text,id_task,list[i].label);
+    for (let i in list) {
+        add_task_html(list[i].title, list[i].text, id_task, list[i].label);
         id_task++;
     }
 }
@@ -107,17 +109,75 @@ function date_today() {
     date.innerHTML = today.toLocaleDateString("eng-ENG", options);
 }
 
-tasks.addEventListener('click',(event)=>{
+tasks.addEventListener('click', (event) => {
     if (event.target.className != 'delete') return;
     let content = event.target.closest('.content');
     content.remove();
     let id_task = event.target.id;
-    list.splice(id_task -1 , 1);
+    list.splice(id_task - 1, 1);
     localStorage.setItem('todo', JSON.stringify(list));
 });
-
-
-
 date_today();
-clean_all_tasks();
+clean_all_tasks()
 
+// ------------------------Sort------------------------
+const red = document.getElementById('red_sort');
+const green = document.getElementById('green_sort');
+const blue = document.getElementById('blue_sort');
+
+red.addEventListener("click", () => {
+    sort_task_func('red');
+});
+green.addEventListener("click", () => {
+    sort_task_func('green');
+});
+blue.addEventListener("click", () => {
+    sort_task_func('blue');
+});
+
+function sort_task_func(color) {
+    sort_tasks.innerHTML = "";
+    let sort_tasks_arr = list.filter((el) => {
+        return el.label == color;
+    })
+    out_all_sort_task(sort_tasks_arr);
+}
+
+function out_all_sort_task(sort_tasks_arr) {
+    let id_task = 0;
+    for (let i in sort_tasks_arr) {
+        add_sort_task_html(sort_tasks_arr[i].title, sort_tasks_arr[i].text, sort_tasks_arr[i].label);
+        id_task++;
+    }
+}
+function add_sort_task_html(title, text, color) {
+    const item = `<div class="content_sort">
+            <div class="content_outside">
+                <div class="title">
+                    ${title}
+                </div>
+                <div class="line ${color}"></div>
+                <div class="text">
+                    ${text}
+                </div>
+            </div>
+        </div>`;
+    const position = 'beforeend';
+    sort_tasks.insertAdjacentHTML(position, item);
+}
+
+// block_select.addEventListener('click', event => {
+//     if (event.target.className != 'button_sort') return;
+//     let id_label = ''
+//     id_label = event.target.id.replace('_sort', ' ');
+//     let id = 0;
+//     for (let i in list){
+//         if (list.label == id_label){
+//             id++
+//         }
+//     }
+//     console.log(id)
+//     // let sort_list = list.filter((value) => {
+//     //     return value.label == id_label;
+//     // });
+// });
